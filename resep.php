aef2998a065eac50d2dce548afe0cad2de6dd682
nikena22 <nikenandini411@gmail.com>
2023-11-01 14:02:05 +0700

    Diubah niken nomer 5 dan 6

--- resep.php
+++ resep.php
@@ -2,8 +2,9 @@
 
 echo "Resep krabby patty <br>";
 echo "================== <br>";
-echo "1.Bawang <br>";
-echo "2.Timun <br>";
-echo "3.keju <br>";
-echo "4.roti <br>";
-echo "5. daging <br>";
+echo "1. Bawang <br>";
+echo "2. Timun <br>";
+echo "3. Keju <br>";
+echo "4. roti <br>";
+echo "5. Daging Sapi <br>";
+echo "6. Saus Tar-tar<br>";
